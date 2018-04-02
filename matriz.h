f95eddb00d496414c1164c7749088b354b08f78d
diogo648 <diogocortez@live.com>
2018-04-02 14:48:34 -0300

    Arquivo inicial matriz.h

---
+++ matriz.h
@@ -0,0 +1,15 @@
+#ifndef MATRIZ_H_
+#define MATRIZ_H_
+
+void DeclararMatriz();
+void DestruirMatriz();
+void ImprimirMatriz();
+void AtribuirEleMatriz();
+void AtribuirLinMatriz();
+void AtribuirColMatriz();
+void TransporMatriz();
+void SomarMatriz();
+void DividirMatriz();
+void MultiplicarMatriz();
+void Mutliplacar2Matrizes();
+
