a4b54e4016d8d95d7e4c68080b9a6e20e005f79e
Marcelino150 <37379562+Marcelino150@users.noreply.github.com>
2018-04-09 14:07:22 -0300

    Update lista.c

--- lista.c
+++ lista.c
@@ -8,16 +8,25 @@ typedef mno No;
 struct mno{
 
 	char nomeMatriz[50];
-	int dimensao[2]; 
+	int dimensao[2];
+	int matriz[][];
 	struct no *prox;
 };
 
-void AdicionarNo(No **mLista,char nomeMatriz[50]){
+void AdicionarNo(No **mLista,char nomeMatriz[50],dimensao[2]){
 
-No *aux = (No*) malloc(sizeof(No));
-
-
-	
+	if(mLista == NULL){
+	No *aux = (No*) malloc(sizeof(No));
+	strcpy(nomeMatriz,aux->nomeMatriz);
+	aux->dimensao[0] = dimensao[0];
+	aux->dimensao[1] = dimensao[1];
+	aux->matriz = DeclararMatriz(dimensao[0],dimensao[1]);
+	aux->prox = NULL;
+	*mLista = aux;
+	}
+	else{
+		AdicionarNo(&(*mLista)->prox,nomeMatriz,dimensao);
+	}
 
 }
 
