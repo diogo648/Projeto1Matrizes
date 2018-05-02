9586edcaa288c8e4e2f67e9edac4bd23a691440a
Marcelino150 <37379562+Marcelino150@users.noreply.github.com>
2018-05-02 16:57:00 -0300

    Update lista.h

--- lista.h
+++ lista.h
@@ -4,8 +4,8 @@
 typedef struct mno No;
 
 void CriaVazia(No **lista);
-void AdicionarNo(No **mLista, char nomeMatriz[50], int linha, int coluna);
-void removerNo(No **mLista, char nomeMatriz[50]);
+void AdicionarNo(No **mLista, char nomeMatriz[], int linha, int coluna);
+void removerNo(No **mLista, char nomeMatriz[]);
 void CriaNova(No **Matriz, int **matrizNova, char nome[], int linha, int coluna);
 No *Percorre(No **Matriz, char nome[]);
 
