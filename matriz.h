4e50270b30c4708128c790d0612b765ea5377ccb
Marcelino150 <37379562+Marcelino150@users.noreply.github.com>
2018-05-02 17:03:54 -0300

    Update matriz.h

--- matriz.h
+++ matriz.h
@@ -1,15 +1,15 @@
 #ifndef MATRIZ_H_
 #define MATRIZ_H_
 
-void DeclararMatriz();
-void DestruirMatriz();
-void ImprimirMatriz();
-void AtribuirEleMatriz();
+int **DeclararMatriz(int linhas, int colunas);
+void DestruirMatriz(No *Matriz, char nome[]);
+void ImprimirMatriz(No **Matriz, char nome[]);
+void AtribuirEleMatriz(No **Matriz, char nome[], int linha, int coluna, int n);
 void AtribuirLinMatriz();
 void AtribuirColMatriz();
-void TransporMatriz();
-void SomarMatriz();
-void DividirMatriz();
-void MultiplicarMatriz();
-void Mutliplicar2Matrizes();
+void TransporMatriz(No **Matriz, char nome[], char nomeResultado[]);
+void SomarMatriz(No **Matriz, char nomePrimeira[], char nomeSegunda[], char Resultante[]);
+void DividirMatriz(No **Matriz, char nomePrimeira[], char nomeSegunda[], char Resultante[]);
+void MultiplicarMatriz(No **Matriz, char nomePrimeira[], char nomeSegunda[], char Resultante[]);
+void Multiplicar2Matrizes(No **Matriz, char nomePrimeira[], char nomeSegunda[], char Resultante[]);
 
